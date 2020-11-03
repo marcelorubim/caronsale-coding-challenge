cf5e8f6920508d6848bad634c6b680be32b3fde5
Marcelo Rubim <marcelorubim@gmail.com>
2020-11-03 05:51:38 -0300

    Implemented the aggregated auction information to be displayed as described on README.md.

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -10,14 +10,24 @@ export class AuctionMonitorApp {
     public constructor(@inject(DependencyIdentifier.LOGGER) private logger: ILogger,
     @inject(DependencyIdentifier.CAR_ON_SALE_CLIENT) private carOnSaleClient: ICarOnSaleClient) {
     }
-
+  
     public async start(): Promise<void> {
+        try {
+            this.logger.log(`Auction Monitor started.`);
 
-        this.logger.log(`Auction Monitor started.`);
-
-        const auctions = await this.carOnSaleClient.getRunningAuctions();
-
-        this.logger.log(JSON.stringify(auctions))
+            const auctions = await this.carOnSaleClient.getRunningAuctions();
+    
+            const averageNumberOfBids = (auctions.reduce((acc, curr) => acc+curr.numBids,0) / auctions.length).toFixed(2)
+    
+            const averagePercentOfAuctionProgress = ((auctions.reduce((acc, curr) => acc+(curr.currentHighestBidValue/curr.minimumRequiredAsk),0) / auctions.length) * 100).toFixed(2)
+    
+            this.logger.log(`Number of actions: ${auctions.length}`)
+            this.logger.log(`Average number of bids: ${averageNumberOfBids}`)
+            this.logger.log(`Average percent auction progress: ${averagePercentOfAuctionProgress}%`)
+        }catch(err){
+            console.error(err)
+            process.exit(1)
+        }
     }
 
 }
